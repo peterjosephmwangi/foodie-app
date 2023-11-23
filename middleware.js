e9b521b149d537cdba0dd5e3ca426e2bce523fab
peterjosephmwangi <mwangijosephpeter@gmail.com>
2023-11-23 07:23:57 +0300

    modified middleware file

--- middleware.js
+++ middleware.js
@@ -2,23 +2,30 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
+  // Create a Next.js response object
   const res = NextResponse.next();
+
+  // Create a Supabase client using the request and response objects
   const supabase = createMiddlewareClient({ req, res });
+
+  // Retrieve the user's session
   const { data } = await supabase.auth.getSession();
 
-  if (data?.session && req.nextUrl.pathname.startsWith("/auth")) {
-    return NextResponse.redirect(new URL("/", req.url));
+  // Check if a session exists and redirect accordingly
+  if (data?.session) {
+    if (req.nextUrl.pathname.startsWith("/auth")) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  } else {
+    // No session: Redirect to "/auth" for specific routes
+    const protectedRoutes = ["/checkout", "/success", "/orders"];
+    if (
+      protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
+    ) {
+      return NextResponse.redirect(new URL("/auth", req.url));
+    }
   }
 
-  // Must be a session to see these routes
-  if (
-    !data?.session &&
-    (req.nextUrl.pathname.startsWith("/checkout") ||
-      req.nextUrl.pathname.startsWith("/success") ||
-      req.nextUrl.pathname.startsWith("/orders"))
-  ) {
-    return NextResponse.redirect(new URL("/auth", req.url));
-  }
-
+  // Return the original response object
   return res;
 }
